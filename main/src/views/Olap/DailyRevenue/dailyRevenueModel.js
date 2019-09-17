5f06e163a532e4487a03c6780025cd1514cbb51f
697668@gmail.com <Rfhfylfi76>
2019-09-17 20:29:46 +1000

    added date range for daily revenue

--- main/src/views/Olap/DailyRevenue/dailyRevenueModel.js
+++ main/src/views/Olap/DailyRevenue/dailyRevenueModel.js
@@ -124,6 +124,15 @@ let dailyRevenueModel = Object.assign(Object.create(olapModelView), {
     },
 
     convertDataToDisplay: function(data) {
+        data.rows.forEach((row, index) => {
+            if (row[COMMENT_COLUMN_START].Value) {
+                row[0].tooltip =
+                    row[COMMENT_COUNT_COLUMN].Value > 1 || index === 0
+                        ? `Комментариев: ${row[COMMENT_COUNT_COLUMN].Value} шт.`
+                        : `${row[COMMENT_COLUMN_START].label}`;
+            }
+        });
+
         if (data.rows.length > 0 && data.rows[0][0].label === 'All') {
             //data.rows[0][0].label = 'Итого';
             data.rows[0].forEach((col, index) => {
@@ -135,15 +144,6 @@ let dailyRevenueModel = Object.assign(Object.create(olapModelView), {
             data.rows.push(data.rows[0]);
             data.rows.shift();
         }
-
-        data.rows.forEach((row, index) => {
-            if (row[COMMENT_COLUMN_START].Value) {
-                row[0].tooltip =
-                    (row[COMMENT_COUNT_COLUMN].Value > 1
-                        ? 'Комментариев: '
-                        : '') + row[COMMENT_COLUMN_START].label;
-            }
-        });
     },
 
     convertDataToExcelFormat: function(data) {
